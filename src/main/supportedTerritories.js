0cb4e233b416278ac64783823dd533dc87e5129f
Katherine Gould <katherine.gould@microsoft.com>
2015-01-22 14:18:18 +0000

    51: first cut of territory configurations

--- src/main/supportedTerritories.js
+++ src/main/supportedTerritories.js
@@ -3,57 +3,382 @@ module.exports = {
     'default': {
         territoryDefaultLanguage: 'default'
     },
+    'ae': {
+        languageOverrides: {
+            'ar': {
+                currencyFormat: "%n %u",
+                currencySymbol: "رس"
+            }
+        },
+        currencyFormat: "%u %n",
+        currencySymbol: "AED",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: "",
+        territoryDefaultLanguage: "en"
+    },
+    'at': {
+        currencyFormat: "%n %u",
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "de"
+    },
+    'au': {
+      currencyFormat: "%u%n",
+      currencySymbol: "$",
+      currencyPrecision: 2,
+      currencySeparator: ".",
+      currencyDelimiter: ",",
+      numberPrecision: 2,
+      numberSeparator: ".",
+      numberDelimiter: ",",
+      territoryDefaultLanguage: "en"
+    },
+    'br': {
+        currencyFormat: "%u%n",
+        currencySymbol: "R$",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "pt"
+    },
+    'ca': {
+        languageOverrides: {
+            'fr': {
+                currencyFormat: "%n %u",
+                currencySeparator: ",",
+                currencyDelimiter: "",
+                numberSeparator: ",",
+                numberDelimiter: ""
+            }
+        },
+        currencyFormat: "%u%n",
+        currencySymbol: "$",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "en"
+    },
+    'ch': {
+        currencyFormat: "%n %u",
+        currencySymbol: "Fr.",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: "'",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: "'",
+        territoryDefaultLanguage: "de"
+    },
+    'cn': {
+        currencyFormat: "%u%n",
+        currencySymbol: "￥",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "zh"
+    },
     'de': {
         currencyFormat: "%n %u",
         currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
         territoryDefaultLanguage: "de"
+    },
+    'es': {
+        currencyFormat: "%n %u",
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "es"
+    },
+    'fi': {
+        currencyFormat: "%n %u",
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: " ",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: " ",
+        territoryDefaultLanguage: "fi"
     },
     'fr': {
         currencyFormat: "%n %u",
         currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: " ",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: " ",
         territoryDefaultLanguage: "fr"
     },
     'gb': {
         currencyFormat: "%u%n",
         currencySymbol: "£",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
         territoryDefaultLanguage: "en"
     },
-    'ca': {
-        languageOverrides: {
-            'fr': {
-                currencyFormat: "%u %n",
-                currencySymbol: "$CAN"
-            }
-        },
-        currencyFormat: "%n%u",
-        currencySymbol: "$",
+    'ie': {
+        currencyFormat: "%u%n",
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
         territoryDefaultLanguage: "en"
     },
-    'vn': {
+    'in': {
+        currencyFormat: "%u%n",
+        currencySymbol: "Rs.",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "en"
+    },
+    'it': {
         currencyFormat: "%n %u",
-        currencySymbol: "₫",
-        currencyPrecision: 0,
-        territoryDefaultLanguage: "vi"
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "it"
+    },
+    'jp': {
+        currencyFormat: "%u%n",
+        currencySymbol: "￥",
+        //currencyPrecision: 2,
+        currencySeparator: "",
+        currencyDelimiter: ",",
+        //numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "ja"
+    },
+    'kr': {
+        currencyFormat: "%n%u",
+        currencySymbol: "€",
+        //currencyPrecision: 2,
+        currencySeparator: "",
+        currencyDelimiter: ",",
+        //numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "ko"
+    },
+    'my': {
+        currencyFormat: "%u%n",
+        currencySymbol: "RM",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "en"
+    },
+    'mx': {
+        currencyFormat: "%u%n",
+        currencySymbol: "$",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "es"
+    },
+    'nl': {
+        currencyFormat: "%u %n",
+        currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: " ",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "nl"
+    },
+    'no': {
+        currencyFormat: "%u %n",
+        currencySymbol: "kr",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".", // should only apply from 10000 onwards
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".", // should only apply from 10000 onwards
+        territoryDefaultLanguage: "nb"
     },
     'pl': {
         currencyFormat: "%n %u",
         currencySymbol: "zł",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: "",
         territoryDefaultLanguage: "pl"
-
-    },
-    'br': {
-        currencyFormat: "%u%n",
-        currencySymbol: "R$",
-        territoryDefaultLanguage: "pt"
     },
     'pt': {
         currencyFormat: "%n %u",
         currencySymbol: "€",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: " ",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: " ",
         territoryDefaultLanguage: "pt"
     },
-    'fi': {
+    'id': {
+        currencyFormat: "%u %n",
+        currencySymbol: "Rp.",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "id"
+    },
+    'ru': {
         currencyFormat: "%n %u",
-        currencySymbol: "€",
-        territoryDefaultLanguage: "fi"
+        currencySymbol: "py6.",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: "",
+        territoryDefaultLanguage: "ru"
+    },
+    'se': {
+        currencyFormat: "%n %u",
+        currencySymbol: "kr",
+        currencyPrecision: 2,
+        currencySeparator: ",", // not needed?
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: "",
+        territoryDefaultLanguage: "sv"
+    },
+    'sg': {
+        currencyFormat: "%u%n",
+        currencySymbol: "$",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "en"
+    },
+    'th': {
+        currencyFormat: "%u%n",
+        currencySymbol: "฿",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "th"
+    },
+    'tr': {
+        currencyFormat: "%n %u",
+        currencySymbol: "TL",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: "",
+        territoryDefaultLanguage: "tr"
+    },
+    'tw': {
+        currencyFormat: "%u%n",
+        currencySymbol: "NT$",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "zh"
+    },
+    'us': {
+        currencyFormat: "%u%n",
+        currencySymbol: "$",
+        currencyPrecision: 2,
+        currencySeparator: ".",
+        currencyDelimiter: ",",
+        numberPrecision: 2,
+        numberSeparator: ".",
+        numberDelimiter: ",",
+        territoryDefaultLanguage: "en"
+    },
+    'vn': {
+        currencyFormat: "%n%u",
+        currencySymbol: "đ",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: ".",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: ".",
+        territoryDefaultLanguage: "vi"
+    },
+    'za': {
+        currencyFormat: "%u%n",
+        currencySymbol: "R",
+        currencyPrecision: 2,
+        currencySeparator: ",",
+        currencyDelimiter: "",
+        numberPrecision: 2,
+        numberSeparator: ",",
+        numberDelimiter: "",
+        territoryDefaultLanguage: "en"
     }
 };
